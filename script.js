7491802dfefcee28c5c252ea7b6d41fdc0c7a817
Hamed050411 <khamed4942@gmail.com>
2025-03-23 17:40:17 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+    //Animation accueil
+function Accueil(){
+    let texteAccueil = document.querySelector("#texte-accueil");
+    let photoAccueil = document.querySelector("#photo-accueil");
+    
+    texteAccueil.style.transform = "translateX(0)";
+    texteAccueil.style.transition = "all 1s ease-in";
+
+    photoAccueil.style.transform = "translateX(0)";
+    photoAccueil.style.transition = "all 2s ease-in";
+
+}
+window.addEventListener("scroll", function (){
+    //Animation a propos
+    let presentation_texte = document.querySelectorAll(".presentation");
+    presentation_texte.forEach(presentation_box => {
+        let position_presentation = presentation_box.getBoundingClientRect().top;
+        let hauterFenetre_presentation = window.innerHeight;
+
+        if(position_presentation < hauterFenetre_presentation && position_presentation > 0 ){
+            presentation_box.style.transform = "translateX(0px)";
+        }
+    })
+    //Animation projet
+    let projet = document.querySelectorAll(".projets .box");
+    projet.forEach(projet_box => {
+        let position_liste = projet_box.getBoundingClientRect().top;
+        let hauterFenetre_liste = window.innerHeight;
+
+        if(position_liste < hauterFenetre_liste && position_liste > 0 ){
+            projet_box.style.transform = "translateY(0px)";
+        }
+    })
+})
