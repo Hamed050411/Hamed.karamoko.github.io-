1416db4f34e7e033469bb72089474fdceb91b003
Hamed050411 <khamed4942@gmail.com>
2025-03-23 23:10:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -23,12 +23,13 @@ window.addEventListener("scroll", function (){
     })
     //Animation projet
     let projet = document.querySelectorAll(".projets .box");
-    projet.forEach(projet_box => {
-        let position_liste = projet_box.getBoundingClientRect().top;
+    let projet_block = document.querySelctorAll(".block-projet");
+    projet_block.forEach(projets_block => {
+        let position_liste = projets_block.getBoundingClientRect().top;
         let hauterFenetre_liste = window.innerHeight;
 
         if(position_liste < hauterFenetre_liste && position_liste > 0 ){
-            projet_box.style.transform = "translateY(0px)";
+            projet.style.transform = "translateY(0px)";
         }
     })
 })
